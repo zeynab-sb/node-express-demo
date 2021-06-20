0fc6b4826929c8cf9d721c5096752bea64db386b
Zeynab Sobhani <zeynab.sobhani6294@gmail.com>
2021-06-20 17:57:20 +0430

    get genres api added

--- index.js
+++ index.js
@@ -1,5 +1,17 @@
 const express = require('express');
 const app = express();
 
+app.use(express.json());
+
+const genres = [
+    {id: 1, name: "Action"},
+    {id: 2, name: "Comedy"},
+    {id: 3, name: "Drama"}
+];
+
+app.get('/api/genres', (req, res) => {
+    res.send(genres);
+});
+
 const port = process.env.PORT || 3000;
 app.listen(port, () => console.log(`Listening on port ${port}`));
