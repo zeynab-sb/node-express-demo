7e28a33d121cc4ffd3ac6d5c2a3d3c6334c39e7a
Zeynab Sobhani <zeynab.sobhani6294@gmail.com>
2021-09-04 21:36:03 +0430

    app restructured

--- index.js
+++ index.js
@@ -1,65 +1,10 @@
 const express = require('express');
 const Joi = require('joi');
 const app = express();
+const genres = require('./routes/genres');
 
 app.use(express.json());
+app.use('/api/genres', genres);
 
-const genres = [
-    {id: 1, name: "Action"},
-    {id: 2, name: "Comedy"},
-    {id: 3, name: "Drama"}
-];
-
-app.get('/api/genres', (req, res) => {
-    res.send(genres);
-});
-
-app.post('/api/genres', (req, res) => {
-    const {error} = validateGenre(req.body);
-    if(error) return res.status(400).send(error.details[0].message);
-
-    const genre = {
-        id: genres.length + 1,
-        name: req.body.name
-    }
-    genres.push(genre);
-
-    res.send(genre);
-});
-
-app.put('/api/genres/:id', (req, res) => {
-    const genre = genres.find( g => g.id === parseInt(req.params.id));
-    if(!genre) return res.status(404).send("The genre with this given ID didn't found.");
-
-    const {error} = validateGenre(req.body);
-    if(error) return res.status(400).send(error.details[0].message);
-
-    genre.name = req.body.name;
-    res.send(genre);
-});
-
-app.delete('/api/genres/:id', (req, res) => {
-    const genre = genres.find( g => g.id === parseInt(req.params.id));
-    if(!genre) return res.status(404).send("The genre with this given ID didn't found.");
-
-    const index = genres.indexOf(genre);
-    genres.splice(index, 1);
-
-    res.send(genre);
-});
-
-app.get('/api/genres/:id', (req, res) => {
-    const genre = genres.find(g => g.id === parseInt(req.params.id));
-    if (!genre) return res.status(404).send('The genre with the given ID was not found.');
-    res.send(genre);
-});
-
-function validateGenre(genre){
-    const schema = Joi.object({
-        name: Joi.string().min(3).required()
-    });
-
-    return schema.validate(genre);
-}
 const port = process.env.PORT || 3000;
 app.listen(port, () => console.log(`Listening on port ${port}`));
