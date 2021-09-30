d6650583d30e18ea4a4d17ca489ac2bbdb36018d
Zeynab Sobhani <zeynab.sobhani6294@gmail.com>
2021-09-30 15:47:22 +0330

    isAdmin field added to user

--- model/user.js
+++ model/user.js
@@ -22,11 +22,12 @@ const userSchema = new mongoose.Schema({
         required: true,
         minlength: 5,
         maxlength: 1024,
-    }
+    },
+    isAdmin: true
 });
 
 userSchema.methods.generateAuthToken = function() {
-    const token = jwt.sign({_id: user._id}, config.get('jwtPrivateKey'));
+    const token = jwt.sign({_id: this._id, isAdmin: this.isAdmin}, config.get('jwtPrivateKey'));
     return token;
 }
 
