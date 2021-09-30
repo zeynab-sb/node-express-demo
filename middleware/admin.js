a097c7cc6820e8694a5e4cc04e4a1de466cd2cf5
Zeynab Sobhani <zeynab.sobhani6294@gmail.com>
2021-09-30 15:54:32 +0330

    admin middleware aded

---
+++ middleware/admin.js
@@ -0,0 +1,8 @@
+
+module.exports = function(req,res,next){
+    
+    if(!req.isAdmin) return res.status(403).send('Access denied.');
+
+    next();
+} 
+
--- routes/genres.js
+++ routes/genres.js
@@ -2,6 +2,7 @@ const express = require('express');
 const router = express.Router();
 const {Genre, validate} = require('../model/genre');
 const auth = require('../middleware/auth');
+const admin = require('../middleware/admin');
 
 router.get('/', async (req, res) => {
     try{
@@ -45,7 +46,7 @@ router.put('/:id', auth, async (req, res) => {
 
 });
 
-router.delete('/:id', auth, async (req, res) => {
+router.delete('/:id', [auth, admin], async (req, res) => {
     try{
         const genre = await Genre.findByIdAndRemove(req.params.id);
         if(!genre) return res.status(404).send("The genre with this given ID didn't found.");
