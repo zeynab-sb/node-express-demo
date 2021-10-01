da52f7ae9ba3ac55c1d43e5b48245b119ad12a8b
Zeynab Sobhani <zeynab.sobhani6294@gmail.com>
2021-10-01 20:48:06 +0330

    validation logic extracted

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 const express = require('express');
 const app = express();
-const Joi = require('joi');
-Joi.objectId = require('joi-objectid')(Joi);
+const winston = require('winston');
 
 require('./startup/logging')()
 require('./startup/routes')(app);
 require('./startup/db')();
 require('./startup/config')();
+require('./startup/validation')();
 
 const port = process.env.PORT || 3000;
-app.listen(port, () => console.log(`Listening on port ${port}`));
+app.listen(port, () => winston.info(`Listening on port ${port}`));
