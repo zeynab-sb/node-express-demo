b48a2c1888ca1dc9c4aae8dfa6ebb095f4a32e27
Zeynab Sobhani <zeynab.sobhani6294@gmail.com>
2021-10-01 18:28:43 +0330

    routes extracted

--- index.js
+++ index.js
@@ -1,19 +1,14 @@
 const express = require('express');
 const app = express();
-const genres = require('./routes/genres');
-const customers = require('./routes/customers');
-const movies = require('./routes/movies');
-const rentals = require('./routes/rentals');
-const users = require('./routes/users');
-const auth = require('./routes/auth');
 const mongoose = require('mongoose');
 const Joi = require('joi');
 Joi.objectId = require('joi-objectid')(Joi);
 const config = require('config');
-const error = require('./middleware/error');
 require('express-async-errors');
 const winston = require('winston');
 require('winston-mongodb');
+
+require('./startup/routes')(app)
 
 winston.handleExceptions(
     new winston.transports.File({filename: 'uncaughtException.log'}));
@@ -36,15 +31,6 @@ if(!config.get('jwtPrivateKey')){
 mongoose.connect('mongodb://localhost/vidly')
     .then(() => console.log("Connected to Mongo..."))
     .catch(err => console.error("Could not connect to Mongo..."));
-
-app.use(express.json());
-app.use('/api/genres', genres);
-app.use('/api/customers', customers);
-app.use('/api/movies', movies);
-app.use('/api/rentals', rentals);
-app.use('/api/users', users);
-app.user('/api/auth', auth);
-app.use(error);
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => console.log(`Listening on port ${port}`));
---
+++ startup/routes.js
@@ -0,0 +1,18 @@
+const genres = require('../routes/genres');
+const customers = require('../routes/customers');
+const movies = require('../routes/movies');
+const rentals = require('../routes/rentals');
+const users = require('../routes/users');
+const auth = require('../routes/auth');
+const express = require('express');
+
+module.exports = function(app){
+    app.use(express.json());
+    app.use('/api/genres', genres);
+    app.use('/api/customers', customers);
+    app.use('/api/movies', movies);
+    app.use('/api/rentals', rentals);
+    app.use('/api/users', users);
+    app.user('/api/auth', auth);
+    app.use(error);
+}
