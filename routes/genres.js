2ce778afab13727a43748509900b3fc21e321ff1
Zeynab Sobhani <zeynab.sobhani6294@gmail.com>
2021-10-03 16:51:52 +0330

    validating objectid added

--- routes/genres.js
+++ routes/genres.js
@@ -3,6 +3,7 @@ const router = express.Router();
 const {Genre, validate} = require('../model/genre');
 const auth = require('../middleware/auth');
 const admin = require('../middleware/admin');
+const mongoose = require('mongoose');
 
 router.get('/', async (req, res) => {
     const genres = await Genre.find();
@@ -55,6 +56,9 @@ router.delete('/:id', [auth, admin], async (req, res) => {
 })
 
 router.get('/:id', async (req, res) => {
+    if(!mongoose.Types.ObjectId.isValid(req.params.id))
+        return res.status(404).send('Invalid ID.');
+
     const genre = await Genre.findById(req.params.id);
     if (!genre) return res.status(404).send('The genre with the given ID was not found.');
     res.send(genre);
