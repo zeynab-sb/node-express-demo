4f182bf0726d41c42472d5e5e43a6dd8bade0677
Zeynab Sobhani <zeynab.sobhani6294@gmail.com>
2021-10-04 15:49:49 +0330

    await added for server.close()

--- tests/integration/auth.test.js
+++ tests/integration/auth.test.js
@@ -7,7 +7,7 @@ let server;
 describe('auth middleware', ()=>{
     beforeEach(() => { server = require('../../index'); })
     afterEach(async () => { 
-        server.close(); 
+        await server.close(); 
         await Genre.remove({});
     });
 
